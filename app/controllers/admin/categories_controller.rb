4a55d22e13bd53af83dc003ae580ab83bf1f16a2
babyhai <hllenoby@gmail.com>
2018-04-24 16:18:45 +0800

    修改新增二级分类

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,34 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
-    @Categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10)
-    .order(id: "desc")
+    if params[:id].blank?
+      @categories = Category.roots
+    else
+      @category = Category.find(params[:id])
+      @categories = @category.children
+    end
+
+    @categories = @categories.page(params[:page] || 1).per_page(params[:per_page] || 10)
+                      .order(id: "desc")
   end
 
+
   def new
+    @category = Category.new
+    @root_categories = Category.roots.order(id: "desc")
   end
+
+
+  def create
+    @category = Category.new(params.require(:category).permit!)
+    @root_categories = Category.roots.order(id: "desc")
+
+    if @category.save
+      flash[:notice] = "保存成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,16 @@
 class Category < ApplicationRecord
   # 分类模型
+  validates :title, presence: { message: "名称不能为空" }
+  validates :title, uniqueness: { message: "名称不能重复" }
+
   has_ancestry
   has_many :prducts, dependent: :destroy
+  before_validation :correct_ancestry
 
+  private
+  def correct_ancestry
+    self.ancestry = nil if self.ancestry.blank?
+  end
 
 
 end
