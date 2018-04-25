d228e701494c349bf4c867a8efe801944897b157
babyhai <hllenoby@gmail.com>
2018-04-25 16:12:14 +0800

    修复问题

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 
 //= require bootstrap-sprockets
+
+//= require jquery-2.1.0
+//= require amazeui.min
+//= require app
+//= require blockUI
+//= require echarts.min
+//= require indexChart
